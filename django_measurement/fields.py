b4374b7be83051154678ecb3612d6084991b5e18
Adam Coddington <me@adamcoddington.net>
2013-01-17 22:21:15 -0800

    Adding pre_init_connect signal listener.

--- django_measurement/fields.py
+++ django_measurement/fields.py
@@ -1,3 +1,4 @@
+from django.db.models import signals
 from django.db.models.fields import CharField, FloatField, CharField
 
 from django_measurement import measure
@@ -50,11 +51,23 @@ class MeasurementField(FloatField):
             **kwargs
         ):
         super(MeasurementField, self).__init__(*args, **kwargs)
+        self.original_unit_field_name = '%s_unit'
+        self.measure_field_name = '%s_measure'
+        self.measurement_field_name = '%s_value'
+
+    def get_original_unit_field_name(self):
+        return self.original_unit_field_name % self.name
+
+    def get_measure_field_name(self):
+        return self.measure_field_name % self.name
+
+    def get_measurement_field_name(self):
+        return self.measurement_field_name % self.name
 
     def contribute_to_class(self, cls, name):
         self.name = name
 
-        original_unit_field_name = '%s_unit' % self.name
+        original_unit_field_name = self.get_original_unit_field_name()
         self.original_unit_field = CharField(
             editable=False,
             blank=True,
@@ -63,7 +76,7 @@ class MeasurementField(FloatField):
         )
         cls.add_to_class(original_unit_field_name, self.original_unit_field)
 
-        measure_field_name = '%s_measure' % self.name
+        measure_field_name = self.get_measure_field_name()
         self.measure_field = CharField(
             editable=False,
             blank=True,
@@ -72,7 +85,7 @@ class MeasurementField(FloatField):
         )
         cls.add_to_class(measure_field_name, self.measure_field)
 
-        measurement_field_name = '%s_value' % self.name
+        measurement_field_name = self.get_measurement_field_name()
         self.measurement_field = CharField(
             editable=False,
             blank=True,
@@ -88,20 +101,20 @@ class MeasurementField(FloatField):
             measure_field_name=measure_field_name,
         )
 
+        signals.pre_init.connect(self.instance_pre_init, sender=cls, weak=False)
+
         setattr(cls, self.name, field)
 
-    def __get__(self, instance, type=None):
-        if instance is None:
-            return self
-        return self.instance_class(instance, self)
-
-    def __set__(self, instance, value):
-        if not isinstance(value, measure.MeasureBase):
-            raise TypeError('')
+    def instance_pre_init(self, signal, sender, args, kwargs, **_kwargs):
+        if self.name in kwargs:
+            value = kwargs.pop(self.name)
+            kwargs[self.get_original_unit_field_name()] = value._default_unit
+            kwargs[self.get_measure_field_name()] = value.__class__.__name__
+            kwargs[self.get_measurement_field_name()] = getattr(value, value.STANDARD_UNIT, 0)
 
 
 try:
     from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^django_measurement\.fields\.(MeasuremeField|MeasurementField|OriginalUnitField)"])
+    add_introspection_rules([], ["^django_measurement\.fields\.MeasurementField"])
 except ImportError:
     pass
