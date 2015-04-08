6927844f77213a10bda77c6bdea2f45442da7cf8
Johannes Hoppe <info@johanneshoppe.com>
2015-04-08 09:11:06 +0200

    Added Django 1.8 tests to travis

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     packages=[
         'django_measurement',
