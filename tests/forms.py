a91d1508251892b1fe5176fd108a2ed5acc1c68a
Ian Price <djianprice@gmail.com>
2016-04-08 14:03:39 -0700

    PEP update (lines)

--- tests/forms.py
+++ tests/forms.py
@@ -4,6 +4,7 @@ from django_measurement.forms import MeasurementField
 from tests.custom_measure_base import DegreePerTime, Temperature, Time
 from tests.models import MeasurementTestModel
 
+
 class MeasurementTestForm(forms.ModelForm):
     class Meta:
         model = MeasurementTestModel
