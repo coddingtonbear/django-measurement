44efedc76063aeccddc4b67852f9f3a940e32f37
Ian Price <djianprice@gmail.com>
2016-04-08 13:45:55 -0700

    Custom MeasureBase subclasses for testing
    Custom MeasureBase subclasses for testing measurements generated from SI_UNITS or LABELS params

---
+++ tests/custom_measure_base.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from sympy import S, Symbol
+from measurement.base import MeasureBase, BidimensionalMeasure
+
+__all__ = [
+    'Temperature',
+    'Time',
+    'DegreePerTime',
+]
+
+
+class Temperature(MeasureBase):
+    SU = Symbol('kelvin')
+    STANDARD_UNIT = 'k'
+    UNITS = {
+        'c': SU - S(273.15),
+        'f': (SU - S(273.15)) * S('9/5') + 32,
+        'k': 1.0
+    }
+    LABELS = {
+        'c': u'°C',
+        'f': u'°F',
+        'k': u'°K',
+    }
+
+
+class Time(MeasureBase):
+    UNITS = {
+        's': 3600.0,
+        'h': 1.0,
+    }
+    SI_UNITS = ['s']
+
+
+class DegreePerTime(BidimensionalMeasure):
+    PRIMARY_DIMENSION = Temperature
+    REFERENCE_DIMENSION = Time
