96a1ef1fb87cfb0bdb772cb85ea1d858214b09ec
Ian Price <djianprice@gmail.com>
2016-04-08 13:44:22 -0700

    Added forms for testing unicode labels

--- tests/forms.py
+++ tests/forms.py
@@ -1,9 +1,22 @@
 from django import forms
-
 from tests.models import MeasurementTestModel
+from django_measurement.forms import MeasurementField
+from .custom_measure_base import Temperature, Time, DegreePerTime
 
 
 class MeasurementTestForm(forms.ModelForm):
     class Meta:
         model = MeasurementTestModel
         exclude = []
+
+
+class LabelTestForm(forms.Form):
+    simple = MeasurementField(Temperature)
+
+
+class SITestForm(forms.Form):
+    simple = MeasurementField(Time)
+
+
+class BiDimensionalLabelTestForm(forms.Form):
+    simple = MeasurementField(DegreePerTime)
