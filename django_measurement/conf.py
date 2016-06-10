e6c054cdb8e953656edfb5cddcc8761753db17bd
Ian Price <djianprice@gmail.com>
2016-06-09 21:38:54 -0700

    Added BIDIMENSIONAL_SEPERATOR setting to appconf
    New conf.py file for Measurement

---
+++ django_measurement/conf.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Settings for django-measurement."""
+from __future__ import absolute_import, unicode_literals
+
+from appconf import AppConf
+from django.conf import settings  # NOQA
+
+__all__ = ('settings', 'DjangoMeasurementConf')
+
+
+class DjangoMeasurementConf(AppConf):
+    """Settings for django-measurement."""
+
+    BIDIMENSIONAL_SEPARATOR = '/'
+    """
+    For measurement classes subclassing a BidimensionalMeasure, this .
+    """
