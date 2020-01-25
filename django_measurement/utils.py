a6df28006d5d347d9d407d6bf46c3cd13a49a967
Omen Apps <support@omenapps.com>
2020-01-24 21:03:53 -0500

    Removed decimal argument is it is not True

--- django_measurement/utils.py
+++ django_measurement/utils.py
@@ -4,9 +4,14 @@ from measurement.base import BidimensionalMeasure
 def get_measurement(measure, value, unit=None, original_unit=None, decimal=False):
     unit = unit or measure.STANDARD_UNIT
 
-    m = measure(
-        **{unit: value}, decimal=decimal
-    )
+    if not decimal:
+        m = measure(
+            **{unit: value}
+        )
+    else:
+        m = measure(
+            **{unit: value}, decimal=decimal
+        )
     if original_unit:
         m.unit = original_unit
     if isinstance(m, BidimensionalMeasure):
