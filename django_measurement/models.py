b5b905fe4dd094679052b23eeea360c050e73c07
Alicja Kozikowska <alicja.kozikowska@thermondo.de>
2021-08-26 15:13:47 +0200

    Replace deprecated ugettext_lazy with gettext_lazy

--- django_measurement/models.py
+++ django_measurement/models.py
@@ -2,7 +2,7 @@ import logging
 import warnings
 
 from django.db.models import FloatField
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from measurement import measures
 from measurement.base import BidimensionalMeasure, MeasureBase
 
