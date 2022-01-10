04f9f172e571a251dee76e0c2e93cc635736b7b4
Nicolás Miñán <nm.imagenysonido@gmail.com>
2022-01-10 18:25:18 -0300

    apply black code formatter

--- django_measurement/rest_framework.py
+++ django_measurement/rest_framework.py
@@ -11,7 +11,7 @@ def is_valid_decimal(value_to_validate):
 
 
 class MeasurementSerializerField(serializers.Field):
-    '''
+    """
     Serializer field to be used with [Django Rest Framework](https://www.django-rest-framework.org/) serializers.
     Could be used with different kind of measures as shown below:
 
@@ -23,7 +23,7 @@ class MeasurementSerializerField(serializers.Field):
         from .models import ProductDimensions
 
 
-        class ProductDimensionsSerializer(serializers.ModelSerializer): 
+        class ProductDimensionsSerializer(serializers.ModelSerializer):
             width = MeasurementSerializerField(measurement_class=Distance, required=False, allow_null=True)
             height = MeasurementSerializerField(measurement_class=Distance, required=False, allow_null=True)
             length = MeasurementSerializerField(measurement_class=Distance, required=False, allow_null=True)
@@ -33,36 +33,33 @@ class MeasurementSerializerField(serializers.Field):
                 model = ProductVariantPhysicalDimensions
                 fields = ["width", "height", "length", "weight"]
     ```
-    '''
+    """
 
     def __init__(self, measurement_class, *args, **kwargs):
         super(MeasurementSerializerField, self).__init__(*args, **kwargs)
         self.measurement_class = measurement_class
 
     def to_representation(self, obj):
-        return {
-            'unit': obj.unit,
-            'value': obj.value
-        }
+        return {"unit": obj.unit, "value": obj.value}
 
     default_error_messages = {
-        'invalid_unit': 'Invalid unit. {invalid_unit} is not a valid {measurement_class.__name__} unit',
-        'invalid_value': 'Invalid value. {invalid_value} is not a valid {measurement_class.__name__} value',
+        "invalid_unit": "Invalid unit. {invalid_unit} is not a valid {measurement_class.__name__} unit",
+        "invalid_value": "Invalid value. {invalid_value} is not a valid {measurement_class.__name__} value",
     }
 
     def to_internal_value(self, data):
-        if not is_valid_unit(data['unit'], self.measurement_class):
+        if not is_valid_unit(data["unit"], self.measurement_class):
             self.fail(
-                'invalid_unit',
-                invalid_unit=data['unit'],
-                measurement_class=self.measurement_class
+                "invalid_unit",
+                invalid_unit=data["unit"],
+                measurement_class=self.measurement_class,
             )
 
-        if not is_valid_decimal(data['value']):
+        if not is_valid_decimal(data["value"]):
             self.fail(
-                'invalid_value',
-                invalid_value=data['value'],
-                measurement_class=self.measurement_class
+                "invalid_value",
+                invalid_value=data["value"],
+                measurement_class=self.measurement_class,
             )
 
-        return self.measurement_class(**{data['unit']: data['value']})
+        return self.measurement_class(**{data["unit"]: data["value"]})
